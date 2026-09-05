4fe83c5ba37d3622041ad2d15432d38e77941175
Paul <pardel@gmail.com>
2026-09-05 11:00:35 +0100

    a slide on its way out goes solid, then fades where it stopped

--- js/deck.js
+++ js/deck.js
@@ -390,7 +390,7 @@
     el.classList.add("leaving");
     el.setAttribute("data-leave", dir);
     el.addEventListener("animationend", done);
-    setTimeout(done, 700);
+    setTimeout(done, 1000);
   }
 
   function show() {
